ea8c700a3cee5809b60b66ee891637d93ab58812
RyanDabest1 <ryanwinn231@gmail.com>
2023-07-09 19:28:21 +0630

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let button = document.querySelector('.btn');
+
+button.addEventListener('mouseover', () => {
+    document.querySelector('.s3').classList.toggle('colorChange')
+    document.querySelector('.title2').classList.toggle('textColorChange')
+    document.querySelector('.description').classList.toggle('textColorChange')
+    let emojis = document.querySelectorAll('i');
+    emojis.forEach((emoji) => {
+        emoji.classList.toggle('emojiColorChange')
+    })
+    
+})
