47b247612289f48d3fd5cd9913a1a75e9e720075
RyanDabest1 <ryanwinn231@gmail.com>
2023-10-15 15:17:54 +0630

    better changes

--- pages/moreInfo/js/moreInfo.js
+++ pages/moreInfo/js/moreInfo.js
@@ -1,7 +1,8 @@
 let background1 = document.querySelector('.mainSec');
 let text1 = document.querySelector('.mainText1');
 
-background1.addEventListener('mouseover', ()=>{
+//Background lightmode to Darkmode Changer
+background1.addEventListener('mouseover', ()=>{ 
     background1.classList.toggle('mainSecChanged')
     text1.classList.toggle('mainText1Changed')
 })
@@ -10,6 +11,7 @@ background1.addEventListener('mouseout', ()=>{
     text1.classList.remove('mainText1Changed')
 })
 
+//Footer closer
 document.addEventListener('click', (e)=>{
   if(e.target.className.indexOf('fa-sharp fa-regular fa-circle-xmark') !== -1 || e.target.className.indexOf('bottomBar__close') !== -1 ){
     document.body.removeChild(e.target.parentElement.parentElement.parentElement)
@@ -17,10 +19,3 @@ document.addEventListener('click', (e)=>{
 
 })
 
-let music = document.querySelector('.backgroundMusic');
-
-document.onclick = playMusic;
-
-function playMusic(){
-  music.play();
-}
